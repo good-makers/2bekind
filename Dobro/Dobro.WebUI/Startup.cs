4b0450c2bc43eeb9589e3d7035e7f0b19b0b582e
John Stoletov <doker971@gmail.com>
2016-03-12 02:10:09 +0300

    visual2 (with non traced files)

---
+++ Dobro/Dobro.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Dobro.WebUI.Startup))]
+namespace Dobro.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
