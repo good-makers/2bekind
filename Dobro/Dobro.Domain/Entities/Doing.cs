f9f9b02d990b9f985d3aeb36c4a72bc4fe52af91
Дмитрий Сердюков Александрович <dimaserd84@gmail.com>
2016-03-15 11:01:07 +0300

    visual_first_commit

---
+++ Dobro/Dobro.Domain/Entities/Doing.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dobro.Domain.Entities
+{
+    public class Doing
+    {
+        public int DoingId { get; set; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Metro { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime FinishTime { get; set; }
+    }
+
+}
